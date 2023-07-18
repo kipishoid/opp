b8aff06f6cd4bc46da2cdc2bb362892c708f0923
[sivolapov_max] <[kipish.max@mail.ru]>
2023-07-18 12:50:43 +0300

    lesson5_1

---
+++ lesson5/calculator/Main.java
@@ -0,0 +1,10 @@
+package lesson5.calculator;
+
+import lesson5.calculator.view.View;
+
+public class Main {
+    public static void main(String[] args) {
+        View view = new View();
+        view.start();
+    }
+}
